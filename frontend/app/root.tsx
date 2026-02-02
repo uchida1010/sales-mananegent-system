4722f6c5b5100bf33a3cdfd085f01bdf4ea9caf9
uchida1010 <tomoki.ksu.0826@gmail.com>
2026-02-02 20:25:36 +0900

    add root.tsx import mantine

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -10,6 +10,9 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
@@ -42,8 +45,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <MantineProvider>
+      <Outlet />
+    </MantineProvider>
+  );
 }
+
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let message = "Oops!";
