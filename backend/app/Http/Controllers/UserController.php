b760ba5a18e6ebdce4351f67fe018a5b1ce84687
uchida1010 <tomoki.ksu.0826@gmail.com>
2026-03-12 19:42:36 +0900

    fix UserController pagination追加

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Data\UserData;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
@@ -11,38 +14,6 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return UserResource::collection(User::paginate(10));
     }
 }
