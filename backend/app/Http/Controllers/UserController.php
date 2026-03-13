48bb3d331e005683ac796b58bd6df47cb07414f9
uchida1010 <tomoki.ksu.0826@gmail.com>
2026-03-13 20:24:30 +0900

    fix UserContrller index以外復元

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Data\UserData;
 use App\Http\Resources\UserResource;
 
 class UserController extends Controller
@@ -15,5 +14,37 @@ class UserController extends Controller
     public function index()
     {
         return UserResource::collection(User::paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
