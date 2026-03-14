ceb956e26b260324d43697fa5e88e5bf06ec0115
uchida1010 <tomoki.ksu.0826@gmail.com>
2026-03-14 18:04:30 +0900

    fix scramble title変更 + openapi生成

--- backend/config/scramble.php
+++ backend/config/scramble.php
@@ -39,7 +39,7 @@ return [
         /*
          * Define the title of the documentation's website. App name is used when this config is `null`.
          */
-        'title' => null,
+        'title' => 'sales-management-system',
 
         /*
          * Define the theme of the documentation. Available options are `light`, `dark`, and `system`.
