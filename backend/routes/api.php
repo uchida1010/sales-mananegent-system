850d9ef7fa4d270863e898ab6c32aee9cb690b0e
uchida1010 <tomoki.ksu.0826@gmail.com>
2026-05-19 21:13:51 +0900

    fix:apiResourceに変更

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::resource("user", UserController::class);
+Route::apiResource("user", UserController::class);
