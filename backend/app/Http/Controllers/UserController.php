f616b671cbe834398a050cd1f730b485425aa915
uchida1010 <tomoki.ksu.0826@gmail.com>
2026-06-09 21:31:20 +0900

    [fix] simplePaginateに修正

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -49,7 +49,7 @@ class UserController extends Controller
             });
         }
 
-        return UserResource::collection($query->get());
+        return UserResource::collection($query->simplePaginate(10));
     }
 
     /**
