14e281882082aefdca3b5b665381d4004b847e9d
uchida1010 <tomoki.ksu.0826@gmail.com>
2026-09-19 19:01:50 +0900

    [feat] ユーザー詳細取得のOpenAPI定義を追加

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -65,9 +65,11 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(User $user)
     {
-        //
+        $user->load('roles');
+
+        return new UserResource($user);
     }
 
     /**
