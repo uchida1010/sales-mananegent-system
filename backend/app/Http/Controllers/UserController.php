c2bb3a7b6c1022ac147fbd615e28b0cd60d7a378
uchida1010 <tomoki.ksu.0826@gmail.com>
2026-09-19 20:02:34 +0900

    [feat]ユーザー削除のOpenAPI定義を追加

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -83,8 +83,10 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return response(null, 204);
     }
 }
